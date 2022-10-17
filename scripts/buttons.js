b914e75cc846115e5cf5986c1ee776a8ec3ea990
Nathan Gilman <48392211+npgilman@users.noreply.github.com>
2022-10-17 14:39:39 -0400

    Created a basic layout for my page using flex.

---
+++ scripts/buttons.js
@@ -0,0 +1,30 @@
+addEventListener("click", (event) => {
+  clicked = event.path[0].id;
+  const bodyElement = document.getElementById("body-container");
+  let newHTML = "<p>Error</p>";
+  let validClick = true;
+
+  switch (clicked) {
+    case "projects_button":
+      newHTML = '<object type="text/html" data="pages/projects.html"></object>';
+      break;
+
+    case "resume_button":
+        newHTML = '<object type="text/html" data="pages/resume.html"></object>';
+      break;
+
+    case "about_button":
+        newHTML = '<object type="text/html" data="pages/about-me.html"></object>';
+      break;
+
+    default:
+      validClick = false;
+      break;
+  }
+
+  if (validClick) {
+    bodyElement.innerHTML = newHTML;
+  } else {
+    console.log("no valid button clicked");
+  }
+});
